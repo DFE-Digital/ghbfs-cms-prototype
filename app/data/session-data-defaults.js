9adeb5eb0bf819d3d74c0565bb8350c5b100a09b
jamiecrisp <47393737+jamiecrisp@users.noreply.github.com>
2021-05-18 15:50:08 +0100

    updated data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -19,6 +19,22 @@ Example usage:
 
 module.exports = {
 
-  // Insert values here
+school: {
+	"contact": [
+	  "email",
+	  "phone"
+	],
+	"contact-by-email": "GeraldStrickland@hillvalley.edu",
+	"contact-by-phone": "0123456789",
+	"procurement-detail": "I need a new catering provider. The current contract ends at the end of the school year. ",
+	"answers-checked": "true",
+	"school-name": "Hill Valley High School",
+	"address-line-1": "Twin Pines street",
+	"address-line-2": "Hill Valley",
+	"address-town": "Hill valley",
+	"address-county": "California",
+	"address-postcode": "",
+	"your-name": "Gerald Strickland"
+	}
 
 }
