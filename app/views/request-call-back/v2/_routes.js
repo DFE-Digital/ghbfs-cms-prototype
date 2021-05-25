5a967a7849fe6c9c98ecd5adcd673b62b88ec521
jamiecrisp <47393737+jamiecrisp@users.noreply.github.com>
2021-05-25 09:43:45 +0100

    added basic API for giabs

--- app/views/request-call-back/v2/_routes.js
+++ app/views/request-call-back/v2/_routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const axios = require("axios");
 
 // Set variables
 router.get('*', function(req, res, next){
@@ -24,6 +25,25 @@ router.post("/type-of-help-post", function(req, res, enxt){
 	} 
 
 });
+
+router.get("/find-school/:query", function(req, res, next){
+
+	let url = `https://scythe-ambiguous-arithmetic.glitch.me/data.json?sql=select+rowid%2C+URN%2C+EstablishmentNumber%2C+EstablishmentName+from+edubasealldata20210521+where+%22EstablishmentName%22+like+%3Ap0+order+by+rowid+limit+10&p0=%25${req.params.query}%25`
+
+	axios.get(url)
+	  .then(function (response) {
+	    // handle success
+	    console.log(response.data.rows);
+	    res.json(response.data.rows);
+	  })
+	  .catch(function (error) {
+	    // handle error
+	    console.log(error);
+	    res.send(error)
+	  })
+});
+
+
 
 
 // Add your routes above the module.exports line
