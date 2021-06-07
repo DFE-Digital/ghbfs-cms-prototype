989472dbf35a416a8587c311f87efaeb9c095176
jamiecrisp <47393737+jamiecrisp@users.noreply.github.com>
2021-06-07 16:37:46 +0100

    accesible autocomplete now working

--- app/views/request-call-back/v3/_routes.js
+++ app/views/request-call-back/v3/_routes.js
@@ -27,22 +27,43 @@ router.post("/type-of-help-post", function(req, res, enxt){
 });
 
 
+router.post("/type-of-establishment-post", function(req, res, enxt){
+
+	if(req.body['type-of-establishment'] === "school"){
+		res.redirect("search-school")
+	}  else if(req.body['type-of-establishment'] === "mat"){
+		res.redirect("search-mat")
+	} else {
+		res.redirect("type-of-establishment");
+	} 
+
+});
+
+
 
 router.get("/find-school/:query", function(req, res, next){
 
-	let url = `https://scythe-ambiguous-arithmetic.glitch.me/data.json?sql=select+rowid%2C+URN%2C+EstablishmentNumber%2C+EstablishmentName+from+edubasealldata20210521+where+%22EstablishmentName%22+like+%3Ap0+order+by+rowid+limit+10&p0=%25${req.params.query}%25`
+	let url = `https://scythe-ambiguous-arithmetic.glitch.me/data.json?sql=select%0D%0A++rowid%2C%0D%0A++URN%2C%0D%0A++EstablishmentNumber%2C%0D%0A++EstablishmentName%2C%0D%0A++Street%2C%0D%0A++Locality%2C%0D%0A++Postcode%0D%0Afrom%0D%0A++edubasealldata20210521%0D%0Awhere%0D%0A++%22EstablishmentName%22+like+%3Ap0%0D%0A++OR+%22URN%22+is+%3Ap0%0D%0Aorder+by%0D%0A++rowid%0D%0Alimit%0D%0A++10` +
+						`&_shape=array&p0=%25${req.params.query}%25`
 
+
+console.log(url)
 	axios.get(url)
 	  .then(function (response) {
 	    // handle success
-	    console.log(response.data.rows);
-	    res.json(response.data.rows);
+	    //console.log(response.data);
+	    let searchResults = []
+	    response.data.forEach( school => searchResults.push(school.EstablishmentName) );
+	    console.log(searchResults)
+	    res.send(searchResults);
 	  })
 	  .catch(function (error) {
 	    // handle error
-	    console.log(error);
+	    //console.log(error);
 	    res.send(error)
 	  })
+
+
 });
 
 
