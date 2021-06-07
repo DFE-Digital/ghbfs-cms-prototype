7db88593d8018b81db4110a48815ea5509fbd404
jamiecrisp <47393737+jamiecrisp@users.noreply.github.com>
2021-06-07 10:20:46 +0100

    copied from v2 to v3

--- app/views/request-call-back/v3/_routes.js
+++ app/views/request-call-back/v3/_routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = new express.Router()
+const axios = require("axios");
 
 // Set variables
 router.get('*', function(req, res, next){
@@ -22,6 +23,47 @@ router.post("/type-of-help-post", function(req, res, enxt){
 	} else {
 		res.redirect("type-of-help");
 	} 
+
+});
+
+
+
+router.get("/find-school/:query", function(req, res, next){
+
+	let url = `https://scythe-ambiguous-arithmetic.glitch.me/data.json?sql=select+rowid%2C+URN%2C+EstablishmentNumber%2C+EstablishmentName+from+edubasealldata20210521+where+%22EstablishmentName%22+like+%3Ap0+order+by+rowid+limit+10&p0=%25${req.params.query}%25`
+
+	axios.get(url)
+	  .then(function (response) {
+	    // handle success
+	    console.log(response.data.rows);
+	    res.json(response.data.rows);
+	  })
+	  .catch(function (error) {
+	    // handle error
+	    console.log(error);
+	    res.send(error)
+	  })
+});
+
+
+router.post("/search-school-post", function(req, res, next){
+
+	let url = `https://scythe-ambiguous-arithmetic.glitch.me/data.json?sql=select%0D%0A++rowid%2C%0D%0A++URN%2C%0D%0A++EstablishmentNumber%2C%0D%0A++EstablishmentName%2C%0D%0A++Street%2C%0D%0A++Locality%2C%0D%0A++Postcode%0D%0Afrom%0D%0A++edubasealldata20210521%0D%0Awhere%0D%0A++%22EstablishmentName%22+like+%3Ap0%0D%0A++OR+%22URN%22+is+%3Ap0%0D%0Aorder+by%0D%0A++rowid%0D%0Alimit%0D%0A++10` +
+						`&_shape=array&p0=%25${req.body['search-school']}%25`
+
+	console.log(url)
+	axios.get(url)
+	  .then(function (response) {
+	    // handle success
+	    console.log(response);
+	    req.session.data.searchResults = response.data;
+	    res.redirect("search-school-results")
+	  })
+	  .catch(function (error) {
+	    // handle error
+	    //console.log(error);
+	    res.send(error)
+	  })
 
 });
 
