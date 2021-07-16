439573cf0458407946d8fb4ad0e24edac2a703b8
jamiecrisp <47393737+jamiecrisp@users.noreply.github.com>
2021-07-16 10:57:23 +0100

    v4 case management

---
+++ app/views/manage-case/v4/_routes.js
@@ -0,0 +1,149 @@
+const express = require('express')
+const router = new express.Router()
+
+const schools = require(`./data/schools.js`);
+const supportInformation = require(`./data/support-information.js`);
+
+let folderVersion = "v4"
+
+// Set variables
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Get help buying for schools'
+
+  // Change the service name URL so it links back this features index
+  res.locals['serviceUrl'] = '/'+req.originalUrl.split('/')[1]+'/'
+
+  next()
+})
+
+router.all('/case/:id/*', function(req, res, next){
+	res.locals.case = schools.find(school => school.id == req.params.id);
+	res.locals.supportInformation = supportInformation.find(item => item.id == req.params.id);
+	console.log(res.locals.supportInformation);
+	next()
+})
+
+
+router.post("/case/:id/call-back/existing-contract-post", function(req, res, next){
+	
+	if(!req.body['existing-contract']){
+		res.redirect(`/manage-case/${folderVersion}/case/${req.params.id}/call-back/existing-contract`)
+	} else if(req.body['existing-contract'] === "yes"){
+		res.redirect(`/manage-case/${folderVersion}/case/${req.params.id}/call-back/contract-details`);
+	} else {
+		req.session.data.tags_existingContract = "true";
+		res.redirect(`/manage-case/${folderVersion}/case/${req.params.id}/call-back/task-list`);
+	}
+
+});
+
+router.post("/:folder/category-post", function(req, res, next){
+	
+	if(!req.body['procurement-category']){
+		res.redirect(`/manage-case/${folderVersion}/${req.params.folder}/category`)
+	} else {
+		req.session.data.tags.category = true;
+		res.redirect(`/manage-case/${folderVersion}/${req.params.folder}#request-details`);
+	}
+
+});
+
+
+router.post("/case/:id/call-back/contract-details-2-post", function(req, res, next){
+	
+
+		req.session.data.tags_existingContract = "true";
+		res.redirect(`/manage-case/${folderVersion}/case/${req.params.id}/call-back/task-list`);
+	
+
+});
+
+
+
+
+router.all("/case-list", function(req, res, next){
+	
+
+	res.locals.cases = schools;
+	next()
+
+})
+
+router.get("/case/:id/:pageName", function(req, res, next){
+
+	res.render(`manage-case/${folderVersion}/case/${req.params.pageName}`)
+
+})
+
+router.get("/case/:id/:folderName/:pageName", function(req, res, next){
+
+	res.render(`manage-case/${folderVersion}/case/${req.params.folderName}/${req.params.pageName}`)
+
+})
+
+router.post("/case/:id/category-post", function(req, res, next){
+	
+	if(!req.body['procurement-category']){
+		res.redirect(`/manage-case/${folderVersion}/case/${req.params.id}/category`)
+	} else {
+		req.session.data.tags.category = true;
+		res.redirect(`/manage-case/${folderVersion}/case/${req.params.id}/new-case`);
+	}
+
+});
+
+router.post("/case/:id/consultation-post", function(req, res, next){
+
+		let school = schools.find(school => school.id == req.params.id);
+		
+		school.status = "Consultation booked"
+
+		school.consultation = req.body;
+	
+		res.redirect(`/manage-case/${folderVersion}/case-list`);
+	
+
+});
+
+router.post("/case/:id/guidance-post", function(req, res, next){
+
+		let school = schools.find(school => school.id == req.params.id);
+		
+		school.status = "closed"
+
+		school["guidance-details"] = req.body["guidance-details"];
+	
+		res.redirect(`/manage-case/${folderVersion}/case-list`);
+	
+});
+
+router.post("/case/:id/call-back/close-case-post", function(req, res, next){
+
+		let school = schools.find(school => school.id == req.params.id);
+		
+		school.status = "closed"
+
+		school["guidance-details"] = req.body["guidance-details"];
+	
+		res.redirect(`/manage-case/${folderVersion}/case-list`);
+	
+});
+router.post("/case/:id/call-back/type-of-procurement-post", function(req, res, next){
+	
+		let school = schools.find(school => school.id == req.params.id);
+		
+		if(!req.body['type-of-procurement']){
+			res.redirect(`/manage-case/${folderVersion}/case/${req.params.id}/call-back/type-of-procurement`);
+		} else {
+			school.status = req.body['type-of-procurement']
+		}
+
+	
+		res.redirect(`/manage-case/${folderVersion}/case-list`);
+	
+});
+
+
+// Add your routes above the module.exports line
+module.exports = router
---
+++ app/views/manage-case/v4/data/schools.js
@@ -0,0 +1,91 @@
+module.exports = [{
+  "id": 1,
+  "first_name": "Tomlin",
+  "last_name": "Trusse",
+  "phone": "7354190016",
+  "email": "ttrusse0@marketwatch.com",
+  "status": "new",
+  "school": {
+    "urn": 100637,
+    "name": "La Retraite Roman Catholic Girls' School",
+    "street": "Atkins Road",
+    "town": "London",
+    "county": null,
+    "postcode": "SW12 0AB"
+  }
+}, {
+  "id": 2,
+  "first_name": "Prent",
+  "last_name": "Angear",
+  "phone": "8393402978",
+  "email": "pangear1@domainmarket.com",
+  "status": "Framework procurement",
+  "school": {
+    "urn": 100243,
+    "name": "Thomas Fairchild Community School",
+    "street": "Forston Street",
+    "town": "London",
+    "county": "Hoxton",
+    "postcode": "N1 7HA"
+  }
+}, {
+  "id": 3,
+  "first_name": "Luella",
+  "last_name": "Biasioni",
+  "phone": "9596913014",
+  "email": "lbiasioni2@nationalgeographic.com",
+  "status": "new",
+  "school": {
+    "urn": 100570,
+    "name": "Hitherfield Junior School",
+    "street": "Leigham Vale",
+    "town": "London",
+    "county": "Streatham",
+    "postcode": "SW16 2JQ"
+  }
+}, {
+  "id": 4,
+  "first_name": "Papageno",
+  "last_name": "Ambrus",
+  "phone": "3983531647",
+  "email": "pambrus3@netlog.com",
+  "status": "Bespoke procurement",
+  "school": {
+    "urn": 100174,
+    "name": "St Alfege with St Peter's Church of England Primary School",
+    "street": "Creek Road",
+    "town": "London",
+    "county": "Greenwich",
+    "postcode": "SE10 9RB"
+  }
+}, {
+  "id": 5,
+  "first_name": "Trescha",
+  "last_name": "Vanyutin",
+  "phone": "8914272157",
+  "email": "tvanyutin4@yolasite.com",
+  "status": "Consultation booked",
+  "school": {
+    "urn": 100373,
+    "name": "Ravenscourt Park Preparatory School",
+    "street": "16 Ravenscourt Avenue",
+    "town": "London",
+    "county": "Ravenscourt Park",
+    "postcode": "W6 0SL"
+  }
+}, {
+  "id": 6,
+  "first_name": "Lorenzo",
+  "last_name": "McGurk",
+  "phone": "2255925977",
+  "email": "lmcgurk5@ucoz.com",
+  "status": "new",
+  "school": {
+    "urn": 100226,
+    "name": "Gayhurst Junior School",
+    "street": "Gayhurst Road",
+    "town": "London",
+    "county": null,
+    "postcode": "E8 3EN"
+  }
+}]
