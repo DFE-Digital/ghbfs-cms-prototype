01ed32d06144f010de754a31aed1782999d7b7de
jamiecrisp <47393737+jamiecrisp@users.noreply.github.com>
2021-08-05 14:36:48 +0100

    added v5

---
+++ app/views/manage-case/v5/data/schools.js
@@ -0,0 +1,166 @@
+let oldData = [{
+  "id": 1,
+  "first_name": "Tomlin",
+  "last_name": "Trusse",
+  "phone": "7354190016",
+  "email": "ttrusse0@marketwatch.com",
+  "status": "new",
+  "receivedDate": "2021-07-2",
+  "lastUpdated": "2021-07-2",
+  "school": {
+    "urn": 100637,
+    "name": "La Retraite Roman Catholic Girls' School",
+    "street": "Atkins Road",
+    "town": "London",
+    "county": null,
+    "postcode": "SW12 0AB"
+  }
+}, {
+  "id": 2,
+  "first_name": "Prent",
+  "last_name": "Angear",
+  "phone": "8393402978",
+  "email": "pangear1@domainmarket.com",
+  "status": "new",
+  "receivedDate": "2021-07-2",
+  "lastUpdated": "2021-07-2",
+  "school": {
+    "urn": 100243,
+    "name": "Thomas Fairchild Community School",
+    "street": "Forston Street",
+    "town": "London",
+    "county": "Hoxton",
+    "postcode": "N1 7HA"
+  }
+}, {
+  "id": 3,
+  "first_name": "Luella",
+  "last_name": "Biasioni",
+  "phone": "9596913014",
+  "email": "lbiasioni2@nationalgeographic.com",
+  "status": "new",
+  "receivedDate": "2021-07-2",
+  "lastUpdated": "2021-07-2",
+  "school": {
+    "urn": 100570,
+    "name": "Hitherfield Junior School",
+    "street": "Leigham Vale",
+    "town": "London",
+    "county": "Streatham",
+    "postcode": "SW16 2JQ"
+  }
+}, {
+  "id": 4,
+  "first_name": "Papageno",
+  "last_name": "Ambrus",
+  "phone": "3983531647",
+  "email": "pambrus3@netlog.com",
+  "status": "new",
+  "receivedDate": "2021-07-2",
+  "lastUpdated": "2021-07-2",
+  "school": {
+    "urn": 100174,
+    "name": "St Alfege with St Peter's Church of England Primary School",
+    "street": "Creek Road",
+    "town": "London",
+    "county": "Greenwich",
+    "postcode": "SE10 9RB"
+  }
+}, {
+  "id": 5,
+  "first_name": "Trescha",
+  "last_name": "Vanyutin",
+  "phone": "8914272157",
+  "email": "tvanyutin4@yolasite.com",
+  "status": "new",
+  "receivedDate": "2021-07-2",
+  "lastUpdated": "2021-07-2",
+  "school": {
+    "urn": 100373,
+    "name": "Ravenscourt Park Preparatory School",
+    "street": "16 Ravenscourt Avenue",
+    "town": "London",
+    "county": "Ravenscourt Park",
+    "postcode": "W6 0SL"
+  }
+}, {
+  "id": 6,
+  "first_name": "Lorenzo",
+  "last_name": "McGurk",
+  "phone": "2255925977",
+  "email": "lmcgurk5@ucoz.com",
+  "status": "new",
+  "receivedDate": "2021-07-2",
+  "lastUpdated": "2021-07-2",
+  "school": {
+    "urn": 100226,
+    "name": "Gayhurst Junior School",
+    "street": "Gayhurst Road",
+    "town": "London",
+    "county": null,
+    "postcode": "E8 3EN"
+  }
+}];
+
+module.exports = [{
+  "id": 1,
+  "first_name": "Leanne ",
+  "last_name": "Jackson",
+  "email": "enquiries@urhazelgrovehigh.co.uk",
+  "status": "new",
+  "school": {
+    "urn": 137923,
+    "name": "Hazel Grove High School",
+    "street": "Hollydale Road",
+    "postcode": "SK7 5JX"
+  },
+  "receivedDate": "2021-07-24",
+  "lastUpdated": "2021-07-24",
+  "phone": "161 549 7700",
+  "specify": true
+}, {
+      "id": 2,
+      "first_name": "Jenny ",
+      "last_name": "Woodhouse",
+      "email": "jenny.woodhouse@urbishopbarrington.net",
+      "status": "Open",
+      "school": {
+         "urn": 100952,
+         "name": "Bishop Barrington School",
+         "postcode": "DL14 6LA"
+      },
+      "receivedDate": "2021-07-26",
+      "lastUpdated": "2021-07-28",
+      "phone": "1388 603307",
+      "specify": true,
+      "history": [
+         {
+            "title": "Reply to school",
+            "caseNote": `Hi Jenny,
+            Thanks for your question. You don't need to fill in the food policy section if your school doesn't currently have a food policy. The specification that you have produced already includes the requirement for service providers to meet the Department for Education standards for school food in England. You can read more about school food standards on GOV.UK:
+
+            https://www.gov.uk/government/publications/standards-for-school-food-in-england
+
+            If your school has requirements that go beyond the Department for Education standards, then including this information can help service providers tailor their responses and could help you find a supplier that best meets your needs.
+            Kind regards
+            Mark`,
+            "date": "28 July 2021",
+            "index": 0
+         }
+      ]
+   }, {
+  "id": 3,
+  "first_name": "Robert ",
+  "last_name": "Nowak",
+  "email": "office@urmonkwickjunior.school",
+  "status": "new",
+  "school": {
+    "urn": 100341,
+    "name": "Monkwick Junior School",
+    "postcode": "CO2 8NN"
+  },
+  "receivedDate": "2021-07-27",
+  "lastUpdated": "2021-07-27",
+  "phone": "1206 575399",
+  "specify": true
+}]
